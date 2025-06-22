8acede4a5ddfd99494f094efeaf9e1e9e18e552b
chaima-amali <chaima.amali@ensia.edu.dz>
2025-06-22 21:58:23 +0100

    Add Instagram logo image to public directory

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,11 @@
 import React from 'react';
 import Navbar from './Navbar';
+import imagehero from '/imagehero.png';
+import facebook from '/facebook.png';
+import LinkedIn from '/LinkedIn.png';
+import github from '/github.png';
+import instagram from '/instagram.png';
+import cv from '/cv.pdf';
 
 export default function Hero() {
   return (
@@ -7,6 +13,44 @@ export default function Hero() {
         <div className='md:h-[550px] h-[500px] w-[450px] bg-gradient-to-r absolute from-[#6d2897] via-[#8e6cf5] to-[#bb61c5] transform rotate-45 z-0 right-2 top-28 rounded-3xl'>
         </div>
         <Navbar />
+    <main id='home' className='flex flex-col md:flex-row items-center justify-center w-full px-4 md:px-52 pb-4 md:pb-24 md:pt-32 pt-24 mt-14 md:mt-0 z-10 '>
+        <section className='flex-1 mr-28 md:text-left mt-10 md:mt-0 relative' data-aos='fade-up' data-aos-delay='500'>
+            <div className='absolute -z-10 w-60 h-60 bg-[#cd3cf5] rounded-full blur-3xl opacity-50 -top-5 -left-12 '> </div>
+                <header>
+                    <h1 className='text-4xl sm:text-4xl md:text-5xl font-bold text-white mb-4 '>Chaima Amali</h1>
+                    <h2 className='text-xl sm:text-4xl md:text-2xl font-bold text-[#3e0f4a] md:text-[#c744ec] mb-2'>
+                    Full-stack Developer
+                    </h2>
+                </header>
+                <p className='text-base sm:text-lg md:text-lg text-gray-200 mb-6'>
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Nihil aut quisquam provident explicabo nam quae, perspiciatis vel delectus in quas?
+                </p>
+                <div className='flex items-center space-x-4 mb-6'>
+                    <a href="#">
+                        <img src={github} alt="github" className='w-9 h-9'/>
+                    </a>
+                    <a href="#">
+                        <img src={LinkedIn} alt="LinkedIn" className='w-9 h-9'/>
+                    </a>
+                    <a href="#">
+                        <img src={instagram} alt="instagram" className='w-9 h-9'/>
+                    </a>
+                    <a href="#">
+                        <img src={facebook} alt="facebook" className='w-9 h-9'/>
+                    </a>
+                </div>
+                <a href={cv} download >
+                    <button className='inline-flex text-white border-2 py-2 px-6 focus:outline-none hover:bg-[#801b9c] hover:shadow-[0_0_40px_rgba(128,0,128,0.7)] rounded-full text-lg'>
+                        Download CV
+                    </button>
+                </a>
+
+            
+        </section>
+        <figure data-aos='fade-up' data-aos-delay='500' className='flex-1 flex justify-center md:justify-end mt-0'>
+            <img src={imagehero} alt="hero image" className='h-[300px] sm:h-[400px] md:h-[485px] w-[250px] sm:w-[480px] object-cover rounded-lg' />
+        </figure>
+    </main>
     </div>    
   )
 }
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,13 +11,13 @@ export default function Navbar() {
     { id:4 ,name: "MY projects", link: "#projects" }
     ]
   return (
-    <header className='fixed top-0 left-0 w-full z-20 text-white' data-aos='fade-up' data-aos-delay='100'>
+    <header className='fixed top-0 left-0 w-full z-20 text-white' data-aos='fade-up' data-aos-delay='300'>
       <div className="container mx-auto flex justify-between items-center p-5">
         {/* LOGO */}
         <a href="#home" className="text-4xl font-bold italic text-white">Portfolio</a>
         
         {/* Mobile Menu Toggle */}
-          <button className="md:hidden focus:outline-none" onClick={() => setIsOpen(!isOpen)} >
+          <button className="md:hidden sm:hidden focus:outline-none" onClick={() => setIsOpen(!isOpen)} >
             <FiMenu className="w-8 h-8 text-white"/>
           </button>
         
@@ -36,7 +36,7 @@ export default function Navbar() {
       </div>
 
         {/* Mobile Navigation */}
-        <div className={`${isOpen ? 'block' : 'hidden'} md:hidden bg--[#801b9c] absolute top-0 left-0 w-full h-screen flex flex-col items-center justify-center space-y-8 pt-16`}>
+        <div className={`${isOpen ? 'block' : 'hidden'} md:hidden bg-[#801b9c] absolute top-0 left-0 w-full h-screen flex flex-col items-center justify-center space-y-8 pt-16`}>
             {/* Close Button */}
             <button className="absolute top-5 right-5 text-white" onClick={() => setIsOpen(false)}>
               <FiX className="w-8 h-8" />
